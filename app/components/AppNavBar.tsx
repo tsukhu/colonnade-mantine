e86f980a25645fa8ad10b75078df3422f2cc4049
Tarun Kumar Sukhu <tksukhu@gmail.com>
2022-06-01 16:54:15 +0530

    update

--- app/components/AppNavBar.tsx
+++ app/components/AppNavBar.tsx
@@ -1,4 +1,4 @@
-import { Navbar } from '@mantine/core';
+import { Box, Navbar, ScrollArea } from '@mantine/core';
 import { User } from './_user';
 import { Brand } from './_brand';
 import { MainLinks } from './_mainLinks';
@@ -9,8 +9,13 @@ export default function AppNavBar({opened}: {opened: boolean}) {
       <Navbar.Section mt="xs">
         <Brand />
       </Navbar.Section>
-      <Navbar.Section grow mt="md">
-        <MainLinks />
+      <Navbar.Section grow component={ScrollArea} mx="-xs" px="xs">
+        <Box py="md">
+          <MainLinks />
+          <MainLinks />
+          <MainLinks />
+          <MainLinks />
+        </Box>
       </Navbar.Section>
       <Navbar.Section>
         <User />
